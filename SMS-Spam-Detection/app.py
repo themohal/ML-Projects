f3f5f6914d07672729c71267a6cbdfb7f19cdb7d
Muhammad Farjad Ali Raza <farjadmohal@gmail.com>
2023-04-04 05:33:17 +0500

    sms-email spam detection

---
+++ SMS-Spam-Detection/app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import pickle
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+import string
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+ps = PorterStemmer()
+
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+def transform_text(text):
+  text = text.lower()
+  text = nltk.word_tokenize(text)
+  new = []
+  for i in text:
+    if i.isalnum():
+      new.append(i)
+  text = new.copy() #deepcopy
+  new.clear()
+  for i in text:
+    if i not in stopwords.words('english') and i not in string.punctuation:
+      new.append(i)
+  text = new.copy()
+  new.clear()
+  for i in text:
+    new.append(ps.stem(i))
+  return " ".join(new)
+
+
+st.title('Email/SMS Spam Classifier')
+
+user_input = st.text_area("Enter the message")
+
+if st.button('Predict'):
+    if user_input!="":
+    #prerprocess text
+        transformed_input = transform_text(user_input)
+    #vectorize
+        vector_input = tfidf.transform([transformed_input])
+    #predict
+        result = model.predict(vector_input)[0]
+    #display
+        if result==1:
+           st.warning('Its a Spam..!')
+        else:
+           
+           st.success('Not Spam..!') 
+    
