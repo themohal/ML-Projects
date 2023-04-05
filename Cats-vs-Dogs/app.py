2aeec9f2e46957adda79900773f966879326a27b
Muhammad Farjad Ali Raza <farjadmohal@gmail.com>
2023-04-05 09:55:53 +0500

    cat vs dog project added

---
+++ Cats-vs-Dogs/app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from PIL import Image
+import io
+import numpy as np
+from tensorflow import keras
+st.title('Cat Vs Dog Classification')
+
+#adding a file uploader
+class_names = ['Cat','Dog']
+model = keras.models.load_model('model.h5')
+
+file = st.file_uploader("Please choose a file")
+
+if file is not None:
+
+    #To read file as bytes:
+
+    bytes_data = file.getvalue()
+    
+    image = Image.open(io.BytesIO(bytes_data))
+    newsize = (256, 256)
+    image_new = image.resize(newsize)
+    image = image_new.convert('RGB')
+    image = np.array(image)
+    test_input = image.reshape((1,256,256,3))
+    
+    
+    result = int(model.predict(test_input)[0][0])
+    st.success("Predicted Class is : "+class_names[result])
+    st.image(image_new)
---
+++ Cats-vs-Dogs/readme.md
@@ -0,0 +1,1 @@
+# Cat Or Dog Classification
